b4d46e394a5c5f4a2ec2751baa23cd05981b33db
Adam Wohlberg <adam.wolhberg@gmail.com>
2014-12-06 07:58:43 -0800

    Set up has_many and belongs_to relationships between todo lists and users along with associated rspec tests to verify

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,16 +2,19 @@ require 'spec_helper'
 
 describe "Viewing todo items" do
   let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
+  let(:user) { create(:user)}
+  before {  sign_in user, password: 'treehouse1' }
 
   it "displays the title of the todo list" do
     visit_todo_list(todo_list)
-    within("h1") do
+    within("div.content h1") do
       expect(page).to have_content(todo_list.title)
     end
   end  
 
   it "displays no items when a todo list is empty" do
     visit_todo_list(todo_list)
+    visit "/todo_lists/#{todo_list.id}/todo_items"
     expect(page.all("ul.todo_items li").size).to eq(0)
   end
 
